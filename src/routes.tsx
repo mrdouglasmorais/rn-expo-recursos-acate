72ac158df07d1cc3f12fc02d9e7fca1c9261f51a
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-08-27 10:12:52 -0300

    update structure

---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default function Routes(){
+  return(
+    <View>
+      <Text>
+        Hello Acate!!!
+      </Text>
+    </View>
+  )
+}
