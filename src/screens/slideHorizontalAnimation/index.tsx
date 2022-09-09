bf8ce364ad9f9c50f1a1636cdb63b2ea835ea778
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-09-08 22:06:59 -0300

    update horizontal animations

--- src/screens/slideHorizontalAnimation/index.tsx
+++ src/screens/slideHorizontalAnimation/index.tsx
@@ -2,28 +2,61 @@ import {
   View,
   Text,
   ScrollView,
-  StyleSheet
+  StyleSheet, 
+  Dimensions
 } from 'react-native';
 
 import Animated, {
-  useSharedValue
+  useSharedValue,
+  interpolateColor,
+  useAnimatedStyle,
+  useAnimatedScrollHandler
 } from 'react-native-reanimated';
 
 import Products from './Products'
-import Card from './Card';
+import Card, {
+  CARD_HEIGHT
+} from './Card';
 import Cards from './components/Cards';
 
-import { products } from './Model'
+import { products } from './Model';
+
+const { width } = Dimensions.get('window');
+
+const snapToOffset = [0, CARD_HEIGHT]
 
 const SlideHorizontalAnimation = () => {
-  const translateX = useSharedValue(0)
+  const translateX = useSharedValue(0);
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      translateX.value = event.contentOffset.x
+    }
+  })
+  const style = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      translateX.value,
+      products.map((_, i) => width * i),
+      products.map((product) => product.color2)
+    ) as string;
+    return { flex: 1, backgroundColor}
+  })
   return(
-    <Animated.View>
-      <Animated.ScrollView>
-        { products.map((product, index) => (
-          <Card product={product} key={index} />
-        ))}
-      </Animated.ScrollView>
+    <Animated.View style={style}>
+      <ScrollView
+        onScroll={onScroll}
+        showsVerticalScrollIndicator={false}
+        snapToOffsets={snapToOffset}
+        snapToEnd={false}
+        decelerationRate="fast"
+      >
+        <View style={styles.slider}>
+          <>
+            { products.map((product, index) => (
+              <Card product={product} key={index} />
+            ))}
+          </>
+        </View>
+      </ScrollView>
       <Products x={translateX} />
       <ScrollView>
         <Cards />
@@ -31,5 +64,11 @@ const SlideHorizontalAnimation = () => {
     </Animated.View>
   )
 }
+
+const styles = StyleSheet.create({
+  slider: {
+    height: CARD_HEIGHT
+  }
+})
 
 export default SlideHorizontalAnimation;
